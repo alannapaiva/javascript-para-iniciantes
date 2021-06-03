62cf4cd19e58c8b0094a1e5afe866f0f312d309b
Alanna Paiva <alannapaiva1@hotmail.com>
2021-06-03 13:48:45 -0300

    variables

---
+++ variaveis/script-aula.js
@@ -0,0 +1,35 @@
+//TIPOS DE DADOS//
+// var nome = 'Alanna' //String
+// var idade = 22; //Number
+// var possuiFaculdade = true; //Boolean
+// var time; //undefined
+// var comida = null; //Null
+// var simbolo = Symbol() //Symbol
+// var novoObjeto = {} //Object
+
+var idade = 28;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Alanna';
+var sobrenome = 'Paiva';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\" jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase2);
---
+++ variaveis/script.js
@@ -0,0 +1,30 @@
+// var nome = 'Andre';
+// var idade = 28;
+// var possuiFaculdade = true;
+
+// console.log(nome, idade, possuiFaculdade);
+
+// var preco = 25;
+// var totalComprado = 5;
+// var totalPreco = totalComprado * preco;
+
+// console.log(totalPreco);
+
+var sobrenome = 'Rafael', 
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var nome = 'Alanna';
+
+var city = 'Sobral';
+
+var food;
+food = 'pasta';
+
+var va1, var2, va3, va4, va5;
+
+console.log( nome,  city, food);
+
+
+
