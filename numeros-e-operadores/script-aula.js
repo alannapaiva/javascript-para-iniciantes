cf07ac1848cc74b3def85f4d077b42bc300f5e5c
Alanna Paiva <alannapaiva1@hotmail.com>
2021-06-03 13:50:49 -0300

    numbers and operators

---
+++ numeros-e-operadores/script-aula.js
@@ -0,0 +1,26 @@
+//Operadores Aritimeticos
+
+// var soma = 100+50; //150
+// var subtracao = 100 - 50; //50
+// var multiplicacao = 100 *2; //200
+// var divisao = 100/2; //50
+// var expoente = 2**4; //16
+// var modulo = 14 % 5; //4
+
+var total1 = 10 + 5 + 10;
+var divisao = 100 / 5;
+var modulo = 3872983892 % 3;
+
+console.log(modulo);
+
+var soma1 = (10 + 10) + 20 + 30 * 4 / (2 + 10);
+console.log(soma1);
+
+var x = 5;
+console.log(x--);
+console.log(x);
+
+var idade = +'28';
+var somaIdade = -5;
+
+console.log(idade);
---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,22 @@
+// qual o resultado da seguinte expressão?
+var total = 10+5*2/2+20;
+
+//Crie duas expressões que retornem Nan
+var exp1 =  'teste'/2;
+var exp2 = 'teste' -2;
+console.log(exp1, exp2);
+
+//Somar a string '200' com o numeros 50 e retornar 250
+var soma = +'200'+50;
+console.log(soma);
+
+//Incremente o numero 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(++x);
+
+//Como dividir o peso por 2?
+var num = +'80' /2;
+var unidade = 'kg';
+var peso = num + unidade; 
+
+console.log(peso);
